75a93c21250d9981af0dfdeb084b90ec0bbe98fc
Carlos Cunha <carlosandrecds@gmail.com>
2024-08-12 00:48:15 -0300

    Initial Commit

---
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by Carlos André Cunha on 12/08/24.
+//
+
+import Testing
+@testable import RickAndMorty
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
